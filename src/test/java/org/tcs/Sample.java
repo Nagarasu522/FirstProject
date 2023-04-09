50f2ae8864cc5e90edd373e7ac5ffe2736d6ff67
Nagarasu522 <nagarasu522@gmail.com>
2023-04-09 14:27:58 +0530

    First time push

---
+++ src/test/java/org/tcs/Sample.java
@@ -0,0 +1,19 @@
+package org.tcs;
+
+
+
+public class Sample {
+	
+
+	private void empName() {
+		System.out.println("Siva");
+		
+		
+	}
+private void empRoll() {
+	System.out.println("423543423");
+
+}
+
+	}
+
